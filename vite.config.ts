50c657b468d3f5946142912f4da23587bd15a07e
Michael Farah <michael.farah@users.noreply.github.com>
2024-05-13 18:29:52 +1000

    chore: add build and TypeScript configuration

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import type { UserConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
+    setupFiles: ["./src/test-setup.ts"],
+  },
+} as VitestConfigExport);
