71d34141ffcf45e4c30c05fee8abbba7a4c407a0
michael-farah <54001683+michael-farah@users.noreply.github.com>
2024-10-07 09:15:32 +0100

    feat: Create basic App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import NavigationPanel from "./components/NavigationPanel";
+import MainContentLayout from "./components/MainContentLayout";
+import ThemeToggle from "./components/ThemeToggle";
+
+const App: React.FC = () => {
+  return (
+    <div className="md:flex">
+      <ThemeToggle />
+      <header className="md:w-1/3 md:h-screen md:sticky md:top-0" role="banner">
+        <NavigationPanel />
+      </header>
+      <main className="w-full md:w-2/3" role="main">
+        <MainContentLayout />
+      </main>
+    </div>
+  );
+};
 
 export default App;
