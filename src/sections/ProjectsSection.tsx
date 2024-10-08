1e3c7f3afbe6910a43e87e17c06d5e18069ab576
michael-farah <54001683+michael-farah@users.noreply.github.com>
2024-10-08 07:35:47 +0100

    fix: Incorrectly named section component

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const AboutSection: React.FC<{}> = () => {
+const ProjectsSection: React.FC<{}> = () => {
   return (
-    <section id="about" className={`flex flex-col items-center`}>
+    <section id="projects" className={`flex flex-col items-center`}>
       <h2 className={`text-5xl font-bold mb-8`}>Projects</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* TODO add projects list component */}
@@ -11,4 +11,4 @@ const AboutSection: React.FC<{}> = () => {
   );
 };
 
-export default AboutSection;
+export default ProjectsSection;
