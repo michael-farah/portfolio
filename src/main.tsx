7e279c9f416dfc561a22595b69b7aaf1e2803dbb
Michael Farah <54001683+michael-farah@users.noreply.github.com>
2026-04-21 18:57:53 +0000

    refactor(main): remove ThemeProvider wrapper
    App no longer needs theme context; render App directly inside
    ErrorBoundary.

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { ThemeProvider } from "./components/ThemeContext";
 import { ErrorBoundary } from "./components/ui/ErrorBoundary";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ErrorBoundary>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+ <App />
     </ErrorBoundary>
   </StrictMode>,
 );
